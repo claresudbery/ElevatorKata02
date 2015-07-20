6dddaeb2a3f317bee56a2ec9539b105057c0764a
Clare Sudbery <clare.sudbery@nice.org.uk>
2015-07-20 14:48:56 +0100

    Forgot to commit this new LiftEngineEvent struct

---
+++ ElevatorKata01/LiftEngineEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace ElevatorKata02
+{
+    public struct LiftEngineEvent
+    {
+        public Direction Direction;
+        public int CurrentFloor;
+        public int LastFloor;
+    }
+}
