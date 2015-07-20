9991de4a4cb6eea075503bd76d6b59e29a2c38c2
Clare Sudbery <clare.sudbery@nice.org.uk>
2015-07-20 15:14:31 +0100

    Added what's needed to allow scheduler to be used for unit tests.

--- ElevatorKata01/LiftEventGenerator.cs
+++ ElevatorKata01/LiftEventGenerator.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Reactive.Concurrency;
 using System.Reactive.Linq;
+using Microsoft.Reactive.Testing;
 
 namespace ElevatorKata02
 {
@@ -7,6 +9,16 @@ namespace ElevatorKata02
     {
         private IDisposable _liftEngineSubscription = null;
         private ObservableLift _lift = null;
+        private IScheduler _scheduler;
+
+        /// <summary>
+        /// Use TestScheduler when instantiating this class in unit tests
+        /// Use Scheduler.Default when instantiating this class in production code
+        /// </summary>
+        public LiftEventGenerator(IScheduler scheduler)
+        {
+            _scheduler = scheduler;
+        }
 
         public IDisposable Subscribe(IObserver<ILiftEvent> observer)
         {
@@ -27,7 +39,8 @@ namespace ElevatorKata02
                     liftStatus => liftStatus.Floor <= lastUpFloor,
                     liftStatus => UpEvent(liftStatus.Floor + 1), // iterator
                     liftStatus => UpEvent(liftStatus.Floor + 1), // actual value? Shouldn't use same val as iterator?
-                    i => TimeSpan.FromMilliseconds(TimeConstants.FloorInterval)
+                    i => TimeSpan.FromMilliseconds(TimeConstants.FloorInterval),
+                    _scheduler
                 );
 
             _liftEngineSubscription = liftEngine.Subscribe
